c4189fe71d1bc2d4a3eae4f203c8be5893a5e2b0
pedroKleinDavila <p.klein@edu.pucrs.br>
2025-10-08 05:52:38 -0300

    fix: clienteRepository null response

--- src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Adaptadores/Dados/ClienteRepositoryJDBC.java
+++ src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Adaptadores/Dados/ClienteRepositoryJDBC.java
@@ -1,6 +1,7 @@
 package com.bcopstein.ex4_lancheriaddd_v1.Adaptadores.Dados;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,7 @@ import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades.Cliente;
 
 @Repository
 public class ClienteRepositoryJDBC implements ClienteRepository {
+
     private JdbcTemplate jdbcTemplate;
 
     @Autowired
@@ -19,14 +21,18 @@ public class ClienteRepositoryJDBC implements ClienteRepository {
     @Override
     public Cliente recuperaPorCpf(String cpf) {
         String sql = "SELECT * FROM clientes WHERE cpf = ?";
-        return jdbcTemplate.queryForObject(sql, new Object[]{cpf}, (rs, rowNum) ->
-                new Cliente(
-                        rs.getString("cpf"),
-                        rs.getString("nome"),
-                        rs.getString("celular"),
-                        rs.getString("endereco"),
-                        rs.getString("email")
-                )
-        );
+        try {
+            return jdbcTemplate.queryForObject(sql, new Object[]{cpf}, (rs, rowNum)
+                    -> new Cliente(
+                            rs.getString("cpf"),
+                            rs.getString("nome"),
+                            rs.getString("celular"),
+                            rs.getString("endereco"),
+                            rs.getString("email")
+                    )
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 }
